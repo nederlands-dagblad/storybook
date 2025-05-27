e80240a71eec25224d61552d88599b4cd5f2b994
Bob Mulder <info@madebybob.nl>
2025-05-27 11:46:32 +0200

    feat: add form input component with stories and styles

---
+++ src/molecules/form/Input/Input.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  value: any,
+  setValue: (value: any) => any,
+  label?: string,
+  help?: string,
+  errors?: string[]|null
+}
+
+const Input: React.FC<InputProps> = (props) => {
+
+  const {label, help, errors } = props;
+
+  function onChange(e) {
+    props.setValue(e.target.value);
+  }
+
+  return (
+    <div className="nd-form-group">
+
+      {label && <label className="label">{label}</label>}
+
+      <input
+        className="nd-form-input"
+        onChange={onChange}
+        {...props}
+      />
+
+      {help && <span className="help">{help}</span>}
+
+      {errors && errors.map((error, i) => (
+        <span key={i} className="error">{error}</span>
+      ))}
+    </div>
+  );
+};
+
+export default Input;
