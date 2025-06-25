2a28943cf9832e2f623210c7770d2f3a79cb4190
Bob Mulder <info@madebybob.nl>
2025-06-25 15:25:44 +0200

    feat: add navigation cards component with styling and stories

---
+++ src/components/portal/NavigationCards/NavigationCard.tsx
@@ -0,0 +1,23 @@
+import { Icon } from "@nederlands-dagblad/storybook";
+
+export interface NavigationCardProps {
+  icon: string
+  headline: string
+  description: string
+  href: string
+}
+
+export default function NavigationCard(props: NavigationCardProps) {
+
+  return (
+    <a className="navigation-card" href={props.href}>
+      <div className="navigation-card-icon">
+        <Icon name={props.icon} />
+      </div>
+      <div className="navigation-card-content">
+        <h3>{props.headline}</h3>
+        <p>{props.description}</p>
+      </div>
+    </a>
+  )
+}
