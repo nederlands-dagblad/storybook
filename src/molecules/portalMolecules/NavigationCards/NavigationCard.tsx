a0abd026b9d19716ec84c9d4a39ec78b8c8b7c28
DewanAlbertsNL <alberts@nd.nl>
2025-08-04 23:05:26 +0200

    fix nav

--- src/molecules/portalMolecules/NavigationCards/NavigationCard.tsx
+++ src/molecules/portalMolecules/NavigationCards/NavigationCard.tsx
@@ -9,18 +9,15 @@ export interface NavigationCardProps {
 
 export function NavigationCard(props: NavigationCardProps) {
     return (
-        <a
-            className="flex flex-row items-center gap-m px-l py-[29px] border border-border-accent-gray-subtle bg-background-default hover:border-border-brand transition-colors duration-200 ease-in-out group"
-            href={props.href}
-        >
-            <div className="shrink-0 w-4 h-4 text-icons-default">
+        <a className="navigation-card" href={props.href}>
+            <div className="navigation-card__icon">
                 <Icon name={props.icon} />
             </div>
-            <div className="flex flex-col items-start justify-start min-w-0 flex-1 gap-xs">
-                <h3 className="text-body-bold text-default group-hover:underline group-hover:[text-underline-offset:25%]">
+            <div className="navigation-card__content">
+                <h3 className="navigation-card__title">
                     {props.headline}
                 </h3>
-                <p className="text-body-light text-default">
+                <p className="navigation-card__description">
                     {props.description}
                 </p>
             </div>
