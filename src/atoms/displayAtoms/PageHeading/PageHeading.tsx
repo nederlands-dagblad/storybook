0fa6f35ab84de518e90849beb73860846ed9bf09
jason-leroux <leroux@nd.nl>
2026-02-12 10:01:49 +0100

    Update heading component to render html

--- src/atoms/displayAtoms/PageHeading/PageHeading.tsx
+++ src/atoms/displayAtoms/PageHeading/PageHeading.tsx
@@ -7,15 +7,18 @@ export type PageHeadingProps = {
 };
 
 export const PageHeading: React.FC<PageHeadingProps> = ({
-    title,
-    showBody = false,
-    bodyText,
-}) => {
+                                                            title,
+                                                            showBody = false,
+                                                            bodyText,
+                                                        }) => {
     return (
         <div>
             <h1 className="text-heading-page text-text-default">{title}</h1>
             {showBody && bodyText && (
-                <p className="text-body-light text-text-default mt-s">{bodyText}</p>
+                <div
+                    className="text-body-light text-text-default mt-s"
+                    dangerouslySetInnerHTML={{ __html: bodyText }}
+                />
             )}
         </div>
     );
