43d02049c6ca67070ef3fefa9f850db9f9783cd0
jason-leroux <leroux@nd.nl>
2026-04-23 09:43:37 +0200

    Update BezorgingStopzetten.tsx

--- src/organisms/MijnNdOrganisms/BezorgingStopzetten/BezorgingStopzetten.tsx
+++ src/organisms/MijnNdOrganisms/BezorgingStopzetten/BezorgingStopzetten.tsx
@@ -230,7 +230,7 @@ export const BezorgingStopzetten: React.FC<BezorgingStopzettenProps> = ({
     }
 
     const successAlert = justSubmitted ? (
-        <Alert>{'Dank je wel voor het doorgeven van je wijziging. Wij verwerken deze in ons systeem. Heb je nog vragen of opmerkingen? Dan kun je <a href="https://nd.nl/service/contact" class="underline">hier contact met ons opnemen</a>.'}</Alert>
+        <Alert>{'Dank je wel voor het doorgeven van je wijziging. Wij verwerken deze in ons systeem en sturen je vervolgens een bevestiging per e-mail. Heb je nog vragen of opmerkingen? Dan kun je <a href="https://nd.nl/service/contact" class="underline">hier contact met ons opnemen</a>.'}</Alert>
     ) : null;
 
     const accordionContent = currentData ? (
